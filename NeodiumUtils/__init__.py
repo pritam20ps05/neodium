acddd9acef403f4d71d5ab49b1533949ac6ee167
Pritam Das <pritamdas.pd2005@gmail.com>
2022-05-29 23:30:58 +0530

    add copyright notice

--- NeodiumUtils/__init__.py
+++ NeodiumUtils/__init__.py
@@ -1,3 +1,16 @@
+"""
+copyright (c) 2021  pritam20ps05(Pritam Das)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+"""
 from .download import Downloader, YTdownload, INSdownload, private_login, ydl_async
 from .prefetch import getCookieFile
 from .helpcommand import NeodiumHelpCommand
